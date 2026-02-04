5d4e8ed3db51c89584c29a6202ecb0f7e59f3a08
prateeksingh132 <kumarpks132@gmail.com>
2026-02-04 15:42:10 -0700

    Adding logic as i go along. moving to models now. starting with user..

--- models/User.js
+++ models/User.js
@@ -1,1 +1,11 @@
 // model: schema for user collection
+
+import mongoose from 'mongoose';
+
+// goal: create user schema
+// i need users so i can link reviews to them later.
+
+const userSchema = new mongoose.Schema({});
+
+
+export default mongoose.model('User', userSchema);
